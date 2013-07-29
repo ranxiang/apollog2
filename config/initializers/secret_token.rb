b931706769e28f3fcf009d14b0f04bb19ffaf87a
Ran Xiang <ranxiang@gmail.com>
2013-07-29 17:32:42 +0800

    first blood

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apollog2::Application.config.secret_key_base = 'e15a112c83d22065024a3b150c1e2c4c34141481b421a9259320848c7907adf85d8f638559b17c6dc347520fc30c8889dccb894ebb5b6aa6c93d9315fdd937f0'
