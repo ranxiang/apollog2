30f937b3165e6e410c25a390ba7987d0f511f387
Ran Xiang <ranxiang@gmail.com>
2013-07-30 16:06:44 +0800

    add scaffold of article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130730080134) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
