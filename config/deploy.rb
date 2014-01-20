3ee94231ff7b94aa0c80752e3cb682d798a576ca
Ran Xiang <ranxiang@gmail.com>
2014-01-21 01:57:56 +0800

    deploy update

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,13 +23,15 @@ set :deploy_to, '/home/ranxiang/deploy/apollog2'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/cache}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { rails_env: "production" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
@@ -43,7 +45,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
