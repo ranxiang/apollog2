ebde025095e6c5fa28aac41fa61d3b7309561f01
Ran Xiang <mail@ranxiang.com>
2015-03-31 14:39:37 +0800

    add link file of secrets.yml for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/ranxiang/deploy/apollog2'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
